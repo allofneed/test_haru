3330f66ca4b79c006378cfa69e30c7b5ab2deed2
NEEDING <allofneeding@gmail.com>
2026-06-01 18:59:46 +0900

    Add files via upload

---
+++ 투개더 프로젝트/app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, jsonify, request
+from livereload import Server
+
+app = Flask(__name__)
+
+@app.route('/')
+def login():
+    return render_template('login.html')
+
+if __name__ == '__main__':
+    app.debug = True
+    server = Server(app.wsgi_app)
+    server.serve(port=5001)
