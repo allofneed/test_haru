6e66eda8aea99d5d2168d6584aca02b8ddc8fb64
your_name <your_email@example.com>
2026-06-02 12:11:18 +0900

    without_live_just_local

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, jsonify, request
-from livereload import Server
 
 app = Flask(__name__)
 
@@ -8,6 +7,7 @@ def login():
     return render_template('login.html')
 
 if __name__ == '__main__':
+    from livereload import Server
     app.debug = True
     server = Server(app.wsgi_app)
     server.serve(port=5003)
