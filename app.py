1d3b79f7b1b67baaf0edafcf8ddfae914f2263d2
your_name <your_email@example.com>
2026-06-04 14:57:40 +0900

    text

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template, jsonify, request, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def login():
     return render_template('login.html')
+
+@app.route('/login', methods=['POST'])
+def login_process():
+    user_input = request.form.get('order_number')
+    print(f"사용자가 입력한 번호 : {user_input}")
+    
+    return redirect(url_for('main_page'))
+
+@app.route('/index')
+def main_page():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     from livereload import Server
