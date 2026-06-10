cb24ec129e3ff6c925644cf1153feec49380b58a
your_name <your_email@example.com>
2026-06-10 15:34:44 +0900

    modal_total

--- static/js/common_modal.js
+++ static/js/common_modal.js
@@ -1,17 +1,48 @@
-document.addEventListener("DOMContentLoaded", () =>{
-  const faqOpenBtn = document.getElementById('btn-open-faq');
+document.addEventListener("DOMContentLoaded", () => {
+  const faqOpenBtn = document.getElementById('btn-faq');
   const faqModal = document.getElementById('faq-modal');
-  const faqCloseBtn = document.getElementById('btn-close-faq');
+
+  const termsOpenBtn = document.getElementById('open-terms');
+  const termsModal = document.getElementById('terms-modal');
+
+  const privacyOpenBtn = document.getElementById('open-privacy');
+  const privacyModal = document.getElementById('privacy-modal');
+
+  const copyrightOpenBtn = document.getElementById('open-copyright');
+  const copyrightModal = document.getElementById('copyright-modal')
 
   if (faqOpenBtn && faqModal) {
-    faqOpenBtn.addEventListener('click', () =>{
+    faqOpenBtn.addEventListener('click', () => {
       faqModal.showModal();
     });
   }
 
-  if (faqCloseBtn && faqModal) {
-    faqCloseBtn.addEventListener('click', () => {
-      faqModal.close();
+  if (termsOpenBtn && termsModal) {
+    termsOpenBtn.addEventListener('click', () =>{
+      termsModal.showModal();
     });
   }
-})
+
+  if (privacyOpenBtn && privacyModal) {
+    privacyOpenBtn.addEventListener('click', () =>{
+      privacyModal.showModal();
+    });
+  }
+
+  if (copyrightOpenBtn && copyrightModal) {
+    copyrightOpenBtn.addEventListener('click', () =>{
+      copyrightModal.showModal();
+    });
+  }
+
+  const closeButton = document.querySelectorAll('#close-modal');
+
+  closeButton.forEach(button => {
+    button.addEventListener('click', () =>{
+      const currentModal = button.closest('dialog');
+      if(currentModal) {
+        currentModal.close();
+      }
+    })
+  })
+});
